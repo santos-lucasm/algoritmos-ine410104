c0daf2e6c96de34ca781bfb37ba72cbee9d62818
lucasmatheus <lucasmatheus@weg.net>
2024-05-10 07:36:50 -0300

    Fix pylsp linting

--- L1/L1_6.py
+++ L1/L1_6.py
@@ -2,6 +2,7 @@ import subprocess
 import math
 import sys
 
+
 def count_occurrences(arr, num):
     count = 0
     for i in arr:
@@ -9,22 +10,24 @@ def count_occurrences(arr, num):
             count += 1
     return count
 
+
 def test_number_pair(n1: int, n2: int):
     res = subprocess.getoutput(f"./tester {n1} {n2}")
     # res = subprocess.getoutput(f"python3 tester-descomprimido.py {n1} {n2}")
-    if(int(res.strip()) == 1):
+    if (int(res.strip()) == 1):
         return 1
     return 0
 
+
 def verify_set(numbers: list):
-    if(len(numbers) == 2):
+    if (len(numbers) == 2):
         return test_number_pair(numbers[0], numbers[1])
 
     left = numbers[:len(numbers)//2]
     right = numbers[len(numbers)//2:]
     left_result = verify_set(left)
     right_result = verify_set(right)
-   
+
     if left_result == right_result:
         return left_result
 
@@ -37,13 +40,14 @@ def verify_set(numbers: list):
         return right_result
     return 0
 
+
 def test_procedure(input_set):
-    if(math.log2(len(input_set)).is_integer() == True):
+    if (math.log2(len(input_set)).is_integer() is True):
         result = verify_set(input_set)
-        return result 
+        return result
     else:
         return 0
 
+
 if __name__ == "__main__":
     print(test_procedure(sys.argv[1:]))
-
